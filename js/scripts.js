f5f8864e78f852f1773b50e3b753f79e03b75415
Jeff Kwon <jykwon007@gmail.com>
2022-02-02 21:26:57 -0800

    Completed task to steer car correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -15,15 +15,21 @@ var carStart;
 
 window.addEventListener('keydown', function (event) {
   var arrow = event.key;
-  if (arrow === ' ' && (carData.started === null || carData.started === false)) {
+  if (arrow === ' ' && carData.direction === 'east' && (carData.started === null || carData.started === false)) {
     carStart = setInterval(function () {
       carPosition.x += 10;
       $car.style.left = carPosition.x + 'px';
     }, 16);
     carData.started = true;
-  } else if (arrow === ' ' && carData.direction === 'north' && carData.started === false) {
-    clearInterval(carStart);
+  } else if (arrow === ' ' && carData.direction === 'north' && (carData.started === null || carData.started === false)) {
     up();
+    carData.started = true;
+  } else if (arrow === ' ' && carData.direction === 'west' && (carData.started === null || carData.started === false)) {
+    left();
+    carData.started = true;
+  } else if (arrow === ' ' && carData.direction === 'south' && (carData.started === null || carData.started === false)) {
+    down();
+    carData.started = true;
   } else if (arrow === ' ' && carData.started === true) {
     clearInterval(carStart);
     carData.started = false;
@@ -33,24 +39,40 @@ window.addEventListener('keydown', function (event) {
     $car.classList.add('north');
     carData.direction = 'north';
     up();
+  } else if (arrow === 'ArrowUp' && carData.started === false) {
+    $car.classList.remove('east', 'south', 'west');
+    $car.classList.add('north');
+    carData.direction = 'north';
   } else if (arrow === 'ArrowRight' && carData.started === true) {
     clearInterval(carStart);
     $car.classList.remove('north', 'south', 'west');
     $car.classList.add('east');
     carData.direction = 'east';
     right();
+  } else if (arrow === 'ArrowRight' && carData.started === false) {
+    $car.classList.remove('north', 'south', 'west');
+    $car.classList.add('east');
+    carData.direction = 'east';
   } else if (arrow === 'ArrowDown' && carData.started === true) {
     clearInterval(carStart);
     $car.classList.remove('north', 'east', 'west');
     $car.classList.add('south');
     carData.direction = 'south';
     down();
+  } else if (arrow === 'ArrowDown' && carData.started === false) {
+    $car.classList.remove('east', 'north', 'west');
+    $car.classList.add('south');
+    carData.direction = 'south';
   } else if (arrow === 'ArrowLeft' && carData.started === true) {
     clearInterval(carStart);
     $car.classList.remove('north', 'south', 'east');
     $car.classList.add('west');
     carData.direction = 'west';
     left();
+  } else if (arrow === 'ArrowLeft' && carData.started === false) {
+    $car.classList.remove('north', 'south', 'west');
+    $car.classList.add('east');
+    carData.direction = 'east';
   }
 });
 
